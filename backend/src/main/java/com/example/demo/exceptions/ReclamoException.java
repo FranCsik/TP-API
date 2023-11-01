eb98fadd4cdd54783224d6340af84d0c1d5b0ebc
Lucca <luccasalerno3@gmail.com>
2023-10-31 21:07:34 -0300

    fix reclamos exception

--- backend/src/main/java/com/example/demo/exceptions/ReclamoException.java
+++ backend/src/main/java/com/example/demo/exceptions/ReclamoException.java
@@ -1,12 +1,10 @@
 package com.example.demo.exceptions;
 
-import java.io.IOException;
-
 public class ReclamoException extends Exception {
 
 	private static final long serialVersionUID = 6646850323041998390L;
 
-	public ReclamoException(String mensaje, IOException e) {
+	public ReclamoException(String mensaje) {
 		super(mensaje);
 	}
 }
